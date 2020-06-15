769f0d074fa69e153897b60f3e4bbf0696aa8f3b
incheol1024 <yaho1024@naver.com>
2020-06-15 22:19:29 +0900

    first commit

---
+++ src/test/java/me/incheol/websocket/StudyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package me.incheol.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
